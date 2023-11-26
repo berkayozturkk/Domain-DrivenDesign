c4e87fc3899b9f8f9732d9fcd1158e50400eb3f4
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-11-26 20:25:52 +0300

    Factories ddd

--- Domain-DrivenDesign.Domain/Users/User.cs
+++ Domain-DrivenDesign.Domain/Users/User.cs
@@ -5,7 +5,7 @@ namespace Domain_DrivenDesign.Domain.Users;
 
 public class User : Entity
 {
-    public User(Guid id,Name name, Email eMail, Password password, Address address)
+    private User(Guid id,Name name, Email eMail, Password password, Address address)
     : base(id)
     {
         Name = name;
@@ -18,6 +18,21 @@ public class User : Entity
     public Email EMail { get; private set; }
     public Password Password { get; private set; }
     public Address Address { get; private set; }
+
+    public static User CreateUser(string name, string email, string password, string country, string city, string street, string postalCode, string fullAddress)
+    {
+        //business ruless
+
+        User user = new(
+            id: Guid.NewGuid(),
+            name: new(name),
+            eMail: new(email),
+            password: new(password),
+            address: new(country, city, street, fullAddress, postalCode));
+
+        return user;
+
+    }
 
     public void ChangeName(string name)
     {
